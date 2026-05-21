277c86b701c761e3406f0c9dc83c53217a731326
fkemeh18 <fkemeh08@gmail.com>
2026-05-21 05:26:49 -0400

    building update
    includes added building placement functionality and signal for ui button

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,13 +1,35 @@
 class_name Game
 extends Node2D
 
-@export var grid: TileMapLayer
+@export var _grid: TileMapLayer
+@export var _building_scene : PackedScene
+@export var _place_building_button: Button
+
+func _ready():
+	_place_building_button.pressed.connect(_button_pressed)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("left_click"):
+		_place_building()
 
 func _process(delta):
+	_grid.set_tile(_get_mouse_grid_pos())
+
+func _get_mouse_grid_pos() -> Vector2i:
+	var grid_pos: Vector2i
 	var mouse_pos = get_global_mouse_position()
 	
-	if grid is TileGrid:
-		var grid_pos = grid.process_mouse_pos(mouse_pos)
-		grid.set_tile(grid_pos)
-		
+	if _grid is TileGrid:
+		grid_pos = _grid.process_mouse_pos(mouse_pos)
 	
+	return grid_pos
+
+func _place_building() -> void:
+	var building = _building_scene.instantiate() as Node2D
+	add_child(building)
+	
+	var grid_pos = _get_mouse_grid_pos()
+	building.global_position = grid_pos * 64
+	
+func _button_pressed():
+	print("hi")
