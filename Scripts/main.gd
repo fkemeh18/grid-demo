633d30ab3b9c6df3683c7e81a2730f259ae8a634
fkemeh18 <fkemeh08@gmail.com>
2026-05-30 23:37:07 -0400

    debug session update
    Ceared some clutter and redundancies, as well as moved certain functions and values around to organize the code and optimize the resulting build a bit.

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,5 +1,5 @@
 class_name Game
-extends Node2D
+extends Node
 
 @export var _main_tml_: MainTML
 @export var _highlight_tml: HighlightTML
@@ -7,7 +7,7 @@ extends Node2D
 @export var _place_building_button: Button
 
 var _temp_grid_pos: Vector2i
-var _active_place_button = false
+var _place_tile_button_active := false
 
 #deals with setting up pressed signal
 func _ready():
@@ -15,42 +15,43 @@ func _ready():
 
 #checks and enacts the pressed signal
 func _unhandled_input(event):
-	if (event.is_action_pressed("left_click") && _main_tml_._visible_tile && (
-		!_main_tml_._occupied_tile_dict.has(_get_mouse_grid_pos()))):
+	if (event.is_action_pressed("left_click") && _place_tile_button_active && (
+		!_main_tml_._occupied_tile_dict.has(_temp_grid_pos))):
 		_place_building()
 
 # passes updated mouse_pos to tile map class for tile
 func _process(delta):
 	_main_tml_.set_tile(_get_mouse_grid_pos())
 	
-	if (_main_tml_._visible_tile && (_temp_grid_pos != null || 
-			_temp_grid_pos != _get_mouse_grid_pos())):
-		_temp_grid_pos = _get_mouse_grid_pos()
+	if (_place_tile_button_active && (_temp_grid_pos != null || 
+			_temp_grid_pos != _get_mouse_grid_pos_without_update())):
 		_highlight_tml._update_highlighted_tiles(_temp_grid_pos)
 
 # checks mouse pos and sends it to the tile grid class
 func _get_mouse_grid_pos() -> Vector2i:
-	var grid_pos: Vector2i
-	var mouse_pos = get_global_mouse_position()
-	
-	grid_pos = _main_tml_.process_mouse_pos(mouse_pos)
-	return grid_pos
+	_temp_grid_pos = _get_mouse_grid_pos_without_update()
+	return _temp_grid_pos
+
+func _get_mouse_grid_pos_without_update() -> Vector2i:
+	var mouse_pos = _highlight_tml.get_global_mouse_position()
+	return _main_tml_.process_mouse_pos(mouse_pos)
 
 #deals with the placement of sprite "building"
 func _place_building() -> void:
+	if _temp_grid_pos == null:
+		return
+	
 	var building = _building_scene.instantiate() as Node2D
 	add_child(building)
+	building.global_position = _temp_grid_pos * 64
 	
-	var grid_pos = _get_mouse_grid_pos()
-	building.global_position = grid_pos * 64
-	_main_tml_._register_built_tile(grid_pos)
-	
-	_main_tml_._toggle_visibility(grid_pos)
+	_main_tml_._register_built_tile(_temp_grid_pos)
+	_main_tml_._toggle_visibility(_temp_grid_pos)
 	_highlight_tml._clear()
-	_active_place_button = false
+	_place_tile_button_active = false
 
 #resolves pressed signal functionality
 func _button_pressed():
-	if !_active_place_button:
-		_active_place_button = true
-		_main_tml_._toggle_visibility(_get_mouse_grid_pos())
+	if !_place_tile_button_active:
+		_place_tile_button_active = true
+		_main_tml_._toggle_visibility(_temp_grid_pos)
